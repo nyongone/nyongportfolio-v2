f840b7938adbd8d7306fd712238a35b3b1850f7e
용원 <auau3@kakao.com>
2024-05-26 00:40:46 +0900

    chore: remove react helmet

--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { GlobalStyles } from "./styles/globals.tsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
     <BrowserRouter>
-      <HelmetProvider>
-        <GlobalStyles />
-        <Routes>
-          <Route path="/" element={<App />} />
-        </Routes>
-      </HelmetProvider>
+      <GlobalStyles />
+      <Routes>
+        <Route path="/" element={<App />} />
+      </Routes>
     </BrowserRouter>
   </>,
 );
